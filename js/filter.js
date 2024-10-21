95b2383bae08846187c5c08896e919f5dfff7787
aleksandr_gvozdev <wayk5i@yandex.ru>
2024-10-22 00:27:13 +0300

    feat: добавляет сортировку по комментариям

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,34 @@
 const filterContainer = document.querySelector('.img-filters');
 const filterButtons = document.querySelectorAll('.img-filters__button');
 
-const fun = (e) => {
+const toggleActiveButton = (evt) => {
   filterButtons.forEach((element) => {
     element.classList.remove('img-filters__button--active');
-    e.target.classList.add('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
   });
 };
 
 
 filterButtons.forEach((element) => {
-  element.addEventListener('click', fun);
+  element.addEventListener('click', toggleActiveButton);
 });
 
-const showFilter = () => {
+const showFilterList = () => {
   filterContainer.classList.remove('img-filters--inactive');
 };
 
-export { showFilter };
+const getCommentsQuantity = (photo) => {
+  const commentsQuantity = photo.comments.length;
+  return commentsQuantity;
+};
+
+const comparePhotosByComments = (photoA, photoB) => {
+  const commentsQuantityA = getCommentsQuantity(photoA);
+  const commentsQuantityB = getCommentsQuantity(photoB);
+  return commentsQuantityB - commentsQuantityA;
+};
+
+const sortByComments = (photoArray) => photoArray.slice().sort(comparePhotosByComments);
+
+
+export { showFilterList, sortByComments };
--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@ import { showLoadAlert } from './alerts.js';
 import { renderPictures } from './render-pictures.js';
 import { renderBigPicture } from './render-big-picture.js';
 import { setFilterFormSubmit } from './open-form.js';
+import { showFilterList, sortByComments } from './filter.js';
 
 getData()
   .then((photos) => {
-    renderPictures(photos);
+    //renderPictures(photos);
+    renderPictures(sortByComments(photos));
     renderBigPicture(photos);
+    showFilterList();
   })
   .catch(() => {
     showLoadAlert();
